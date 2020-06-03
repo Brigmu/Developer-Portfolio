e1edde817b7ffe3dd3576a2b8f1ff898ce3baf4c
Brigham Mueller <brigmu@gmail.com>
2020-06-02 21:30:55 -0700

    pulled files out of main folder

---
+++ build/precache-manifest.8cf5fc1be4edeb5c8d86baa29fa9e28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135e7dd07339b732f328075c54ee6efc",
+    "url": "/Developer-Portfolio/index.html"
+  },
+  {
+    "revision": "a248c44f810eef382153",
+    "url": "/Developer-Portfolio/static/css/main.9155f96f.chunk.css"
+  },
+  {
+    "revision": "902ee34911645f88ca07",
+    "url": "/Developer-Portfolio/static/js/2.6dbd84e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Developer-Portfolio/static/js/2.6dbd84e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a248c44f810eef382153",
+    "url": "/Developer-Portfolio/static/js/main.b65c37e1.chunk.js"
+  },
+  {
+    "revision": "5fa5116b1b4e65144109",
+    "url": "/Developer-Portfolio/static/js/runtime-main.435777f5.js"
+  }
+]);
